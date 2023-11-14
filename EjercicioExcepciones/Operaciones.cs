5eeaa388f29151bb7b85c8f47f894e319a954d44
andres-rivera-05 <cristianrivera161141@gmail.com>
2023-11-13 21:27:10 -0600

    commit uno

---
+++ EjercicioExcepciones/Operaciones.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioExcepciones
+{
+    internal class Operaciones
+    {
+        public double Suma(double N1, double N2) { 
+            return N1 + N2; 
+        }
+
+        public double Resta(double N1, double N2)
+        {
+            return N1 - N2;
+        }
+
+        public double Multipicacion(double N1, double N2)
+        {
+            return N1 * N2;
+        }
+
+        public double Divicion(double N1, double N2)
+        {
+
+            if (N2 == 0) {
+                throw new DivideByZeroException();
+            }
+            return N1 / N2;
+        }
+    }
+}
--- EjercicioExcepciones/Program.cs
+++ EjercicioExcepciones/Program.cs
@@ -1,2 +1,42 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using EjercicioExcepciones;
+
 Console.WriteLine("Hello, World!");
+
+
+double num1, num2, suma, resta, divicion, multiplicacion;
+
+try {
+
+    Console.WriteLine("Ingrese un numero: ");
+    num1 = PedirNumero();
+
+    Console.WriteLine("Ingrese un numero: ");
+    num2 = PedirNumero();   
+
+
+    Operaciones operaciones = new Operaciones();
+
+    suma = operaciones.Suma(num1, num2);
+    resta = operaciones.Resta(num1, num2);  
+    multiplicacion = operaciones.Multipicacion(num1, num2);  
+
+
+
+
+
+
+}catch (Exception) { }    
+
+
+static double PedirNumero() { 
+
+   string input = Console.ReadLine();
+
+    if (!double.TryParse(input, out double numero)) {
+
+        throw new FormatException();
+    }
+
+    return numero;
+}
